cacf923a3961b4951a54b785b2175fa29b525f13
Annie <zilinca051@gmail.com>
2025-09-13 16:19:41 +0200

    reponse Fixtures

---
+++ src/DataFixtures/ReponseFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Question;
+use App\Entity\Reponse;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class ReponseFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+      $faker = Faker\Factory::create('ro_RO');
+      $repository = $manager->getRepository(Question::class);
+      $question = $repository->findAll();
+      
+        for ($i = 0; $i < 50; $i++){
+            $reponse = new Reponse();
+            $reponse->setTexte($faker->sentence);
+            $reponse->setEstCorrecte($faker->boolean);
+            $reponse->setQuestion($faker->randomElement($question));
+            $manager->persist($reponse);
+        }
+
+        $manager->flush();
+    }
+}
