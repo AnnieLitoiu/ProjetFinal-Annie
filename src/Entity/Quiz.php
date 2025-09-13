886555bc39c49ce91af793f0b3aa116af7e8414b
Annie <zilinca051@gmail.com>
2025-09-13 12:21:50 +0200

    ajouter les entites: categorie,question,reponse

--- src/Entity/Quiz.php
+++ src/Entity/Quiz.php
@@ -27,9 +27,19 @@ class Quiz
     #[ORM\OneToMany(targetEntity: Tentative::class, mappedBy: 'quiz')]
     private Collection $tentatives;
 
+    /**
+     * @var Collection<int, Question>
+     */
+    #[ORM\OneToMany(targetEntity: Question::class, mappedBy: 'quiz')]
+    private Collection $questions;
+
+    #[ORM\ManyToOne(inversedBy: 'quiz')]
+    private ?Categorie $categorie = null;
+
     public function __construct()
     {
         $this->tentatives = new ArrayCollection();
+        $this->questions = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -87,6 +97,48 @@ class Quiz
                 $tentative->setQuiz(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Question>
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): static
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions->add($question);
+            $question->setQuiz($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): static
+    {
+        if ($this->questions->removeElement($question)) {
+            // set the owning side to null (unless already changed)
+            if ($question->getQuiz() === $this) {
+                $question->setQuiz(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): static
+    {
+        $this->categorie = $categorie;
 
         return $this;
     }
