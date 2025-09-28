71fde1763544174ab2eceb90bbedf3975d7b8ca1
Annie <zilinca051@gmail.com>
2025-09-28 12:05:25 +0200

    alternative requette quiz

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NiveauRepository;
 use App\Repository\QuizRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +12,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class QuizController extends AbstractController
 {
     #[Route('/quiz/liste/{niveau}', name: 'app_quiz_liste')]
-    public function quizListe(Request $req, QuizRepository $rep): Response
+    public function quizListe(Request $req, QuizRepository $rep, NiveauRepository $repNiveau): Response
     {
         $idNiveau = $req->get('niveau');
-       
-        $vars = ['quiz' => $rep->findBy(['niveau' => $idNiveau])];
+
+        $vars = ['quiz' => $repNiveau->find($idNiveau)->getQuizzes()];
+
+        // $vars = ['quiz' => $rep->findBy(['niveau' => $idNiveau])];
 
         return $this->render('quiz/liste.html.twig', $vars);
     }
