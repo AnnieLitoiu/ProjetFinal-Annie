b3f7fbad4e0d817df7c45c333fcad760d1134666
Annie <zilinca051@gmail.com>
2025-10-07 11:05:37 +0200

    regler bouton fin quiz + creer fichier css/style + regler affichage liste de quiz

--- migrations/Version20251007071655.php
+++ migrations/Version20251007071655.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251005181304 extends AbstractMigration
+final class Version20251007071655 extends AbstractMigration
 {
     public function getDescription(): string
     {
