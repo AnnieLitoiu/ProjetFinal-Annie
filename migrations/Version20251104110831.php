97b2d1693558825cb7c60040f5f461895d4dff37
Annie <zilinca051@gmail.com>
2025-11-04 13:21:57 +0100

    idees a ajouter cote utilisateur

--- migrations/Version20251104110831.php
+++ migrations/Version20251104110831.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251017221058 extends AbstractMigration
+final class Version20251104110831 extends AbstractMigration
 {
     public function getDescription(): string
     {
