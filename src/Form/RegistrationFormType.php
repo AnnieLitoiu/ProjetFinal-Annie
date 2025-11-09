9b9a3ae7a7042a8fb16f57ace33eecf37dbc0f36
Annie <zilinca051@gmail.com>
2025-11-09 15:24:45 +0100

    mise à jour templates register et login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -15,17 +17,31 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'required' => false,
+                'attr' => ['placeholder' => 'Entrez votre prénom'],
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'required' => false,
+                'attr' => ['placeholder' => 'Entrez votre nom'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'attr' => ['placeholder' => 'exemple@email.com'],
+            ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         'max' => 4096,
                     ]),
                 ],
