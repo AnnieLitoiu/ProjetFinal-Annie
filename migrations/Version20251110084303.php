2b84851a7e78a503638b49092a212e3f813e4931
Annie <zilinca051@gmail.com>
2025-11-10 09:48:06 +0100

    Sauvegarde locale avant pull

--- migrations/Version20251110084303.php
+++ migrations/Version20251110084303.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251104110831 extends AbstractMigration
+final class Version20251110084303 extends AbstractMigration
 {
     public function getDescription(): string
     {
