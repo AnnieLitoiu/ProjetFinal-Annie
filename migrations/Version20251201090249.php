877db8f425533010255a477b5b2b449e5eed07de
Annie <zilinca051@gmail.com>
2025-12-01 12:51:26 +0100

    update server

--- migrations/Version20251201090249.php
+++ migrations/Version20251201090249.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251130231059 extends AbstractMigration
+final class Version20251201090249 extends AbstractMigration
 {
     public function getDescription(): string
     {
