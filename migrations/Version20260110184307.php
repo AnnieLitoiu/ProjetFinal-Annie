a8d24d69237938e2babdad9db882bdee44d3f15c
Annie <zilinca051@gmail.com>
2026-01-11 12:47:28 +0100

    modifier vue page /mon-activite en cards

---
+++ migrations/Version20260110184307.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260110184307 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE niveau (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, quiz_id INT DEFAULT NULL, enonce VARCHAR(255) NOT NULL, INDEX IDX_B6F7494E853CD175 (quiz_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE quiz (id INT AUTO_INCREMENT NOT NULL, niveau_id INT NOT NULL, titre VARCHAR(255) NOT NULL, INDEX IDX_A412FA92B3E9C81 (niveau_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reponse (id INT AUTO_INCREMENT NOT NULL, question_id INT DEFAULT NULL, texte VARCHAR(255) NOT NULL, est_correcte TINYINT(1) NOT NULL, INDEX IDX_5FB6DEC71E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tentative (id INT AUTO_INCREMENT NOT NULL, quiz_id INT NOT NULL, utilisateur_id INT DEFAULT NULL, max_tentatives INT NOT NULL, date_debut DATETIME NOT NULL, date_fin DATETIME DEFAULT NULL, reponses_correctes INT DEFAULT NULL, reponses_mauvaises INT DEFAULT NULL, reponses_donnees INT DEFAULT NULL, non_repondues INT DEFAULT NULL, pourcentage NUMERIC(10, 0) DEFAULT NULL, reponses_utilisateur JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', nombre_questions INT DEFAULT NULL, temps_alloue_secondes INT DEFAULT NULL, question_ids JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', INDEX IDX_DBC382F9853CD175 (quiz_id), INDEX IDX_DBC382F9FB88E14F (utilisateur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, prenom VARCHAR(100) DEFAULT NULL, nom VARCHAR(100) DEFAULT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1D1C63B3E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id)');
+        $this->addSql('ALTER TABLE quiz ADD CONSTRAINT FK_A412FA92B3E9C81 FOREIGN KEY (niveau_id) REFERENCES niveau (id)');
+        $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC71E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE tentative ADD CONSTRAINT FK_DBC382F9853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id)');
+        $this->addSql('ALTER TABLE tentative ADD CONSTRAINT FK_DBC382F9FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E853CD175');
+        $this->addSql('ALTER TABLE quiz DROP FOREIGN KEY FK_A412FA92B3E9C81');
+        $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC71E27F6BF');
+        $this->addSql('ALTER TABLE tentative DROP FOREIGN KEY FK_DBC382F9853CD175');
+        $this->addSql('ALTER TABLE tentative DROP FOREIGN KEY FK_DBC382F9FB88E14F');
+        $this->addSql('DROP TABLE niveau');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE quiz');
+        $this->addSql('DROP TABLE reponse');
+        $this->addSql('DROP TABLE tentative');
+        $this->addSql('DROP TABLE utilisateur');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
